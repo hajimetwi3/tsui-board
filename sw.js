fe089aa628d6ca5853b58d07265fcc43bf35bc23
ついはじめ ( Hajime Tsui ) <247918659+hajimetwi3@users.noreply.github.com>
2026-04-22 21:53:28 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+'use strict';
+const CACHE = 'tsui-board-v1.0.0';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.webmanifest',
+  './icon.svg',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((ks) =>
+      Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
